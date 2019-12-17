86fcf11cd5b12361776074ae64053c41cb65d588
Diego Mucheniski <mucheniski@gmail.com>
2019-12-17 10:48:30 -0300

    Teste da pagina 127

---
+++ src/pagina127/IncrementoDecremento.java
@@ -0,0 +1,12 @@
+package pagina127;
+
+public class IncrementoDecremento {
+	
+	public static void main(String[] args) {
+		int j = 0;
+		int i = (j++ * j + j++);
+		System.out.println(i);
+		System.out.println(j);
+	}
+
+}
