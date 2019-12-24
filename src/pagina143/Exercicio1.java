bec177aa4994174f499c069745191ebf52956ce5
mucheniski <mucheniski@NoteDiego>
2019-12-24 17:24:34 -0300

    Exercicios pagina 143

---
+++ src/pagina143/Exercicio1.java
@@ -0,0 +1,15 @@
+package pagina143;
+
+/*1) Escolha a opção adequada ao tentar compilar e rodar o
+arquivo a seguir:*/
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+//		String result = ("division: " + 15) / 0.0;
+//		System.out.println(result);
+	}
+
+}
+
+// a) Não compila.
---
+++ src/pagina143/Exercicio2.java
@@ -0,0 +1,13 @@
+package pagina143;
+/*2) Escolha a opção adequada ao tentar compilar e rodar o
+arquivo a seguir:*/
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		System.out.println(((!(true == false)) == true ? 1 : 0) == 0);
+	}
+
+}
+
+// b) Imprime false
